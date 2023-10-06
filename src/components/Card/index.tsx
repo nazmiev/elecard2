f184fa9f61d1caa63b3c63395de3b9ef671bc211
Рома <57133971+nazmiev@users.noreply.github.com>
2023-10-07 00:50:06 +0700

    feat(date): date added

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,7 +3,7 @@ import { CardProps } from '../../redux/card/types';
 import { useAppDispatch } from '../../redux/store';
 import styles from './Card.module.scss';
 
-const Card: React.FC<CardProps> = ({ image }) => {
+const Card: React.FC<CardProps> = ({ image, timestamp }) => {
     const dispatch = useAppDispatch();
 
     const onClosedPic = (image: string) => {
@@ -16,12 +16,12 @@ const Card: React.FC<CardProps> = ({ image }) => {
         }
         localStorage.setItem('closedPics', JSON.stringify(new Set(image)))
     }
-
+    const date = new Date(timestamp).toDateString();
     return (
         <article key={image} className={styles.card}>
             <button type="button" onClick={() => onClosedPic(image)}>X</button>
             <img src={`http://contest.elecard.ru/frontend_data/${image}`} />
-            <p>Yep, just some simple content ecapsulated in this card</p>
+            <p>{date}</p>
         </article>
     );
 }
